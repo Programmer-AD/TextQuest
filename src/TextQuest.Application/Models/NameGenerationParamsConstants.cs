43c5f60c20e8781a259250f4bee401992d5c99ea
Andrey Dmytrenko <andrey08112003@gmail.com>
2022-03-14 21:04:35 +0200

    Changed name genration params

--- src/TextQuest.Application/Models/NameGenerationParamsConstants.cs
+++ src/TextQuest.Application/Models/NameGenerationParamsConstants.cs
@@ -2,9 +2,9 @@
 {
     internal static class NameGenerationParamsConstants
     {
-        public static readonly NameGenerationParams WorldName = new(1..2, 3..5);
-        public static readonly NameGenerationParams LocationName = new(1..3, 3..5);
+        public static readonly NameGenerationParams WorldName = new(1..2, 2..4);
+        public static readonly NameGenerationParams LocationName = new(1..3, 1..4);
         public static readonly NameGenerationParams CharacterName = new(2..2, 2..4);
-        public static readonly NameGenerationParams ItemName = new(1..3, 2..5);
+        public static readonly NameGenerationParams ItemName = new(1..3, 2..4);
     }
 }
